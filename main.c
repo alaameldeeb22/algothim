a176a6e36507eeac5770d3ac0e61d543e248800e
alaameldeeb22 <105177463+alaameldeeb22@users.noreply.github.com>
2022-10-01 00:18:22 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,80 @@
 #include <stdio.h>
-int BinarySearch(int arr[], int low, int high, int element){
-   if (high >= low){
-      int middle =  (high + low)/2;
-      if (arr[middle] == element)
-         return middle;
-      if (arr[middle] > element)
-         return BinarySearch(arr, low, middle-1, element);
-      return BinarySearch(arr, middle+1, high, element);
-   }
-   return -1;
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
-int main(void){
-   int arr[] = {1, 4, 7, 9, 16, 56, 70};
-   int n = sizeof(arr) / sizeof(arr[0]);
-    printf("enter the num ");
-    int x ;
-    scanf("%d",&x);
-   int found_index = BinarySearch(arr, 0, n-1, x);
-   if(found_index == -1 ) {
-      printf("Element not found in the array ");
-   }
-   else {
-      printf("Element found at index : %d",found_index);
-   }
-   return 0;
+
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+
+        int m = l + (r - l) / 2;
+
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
 }
